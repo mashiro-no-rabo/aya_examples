e40d92a2cc2750c37e2ed666357c90b1276cb9ce
LOU Xun <aquarhead@ela.build>
2021-06-18 13:12:16 +0000

    Configure rustflags to avoid LLVM generate bad reloc

--- test.sh
+++ test.sh
@@ -3,9 +3,9 @@
 set -ex
 
 (cd kprob && cargo rustc --release -- \
-        -C linker-plugin-lto \
-        -C linker-flavor=wasm-ld -C linker=bpf-linker \
-        -C link-arg=--target=bpf)
+  -C linker-plugin-lto \
+  -C linker-flavor=wasm-ld -C linker=bpf-linker \
+  -C link-arg=--target=bpf)
 
 (cd deployable && cargo build)
 
